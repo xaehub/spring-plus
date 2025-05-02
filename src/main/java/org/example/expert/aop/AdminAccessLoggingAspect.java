9eb528ee75704f5633a0625e668bad652bfe8e5c
xaehub <novxae@gmail.com>
2025-05-02 19:28:51 +0900

    필수 Lv.5: 코드 개선 퀴즈 - AOP의 이해 - changeUserRole 메서드 실행 전에 동작하도록 changeUserRole 메서드 위치로 수정

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -19,7 +19,7 @@ public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
 
-    @Before("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
+    @Before("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))")
     public void logAfterChangeUserRole(JoinPoint joinPoint) {
         String userId = String.valueOf(request.getAttribute("userId"));
         String requestUrl = request.getRequestURI();
